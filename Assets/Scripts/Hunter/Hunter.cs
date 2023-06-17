32f0bd4371242fcf67a2b7645884df6e2fb1d059
Nico Reale <nicor_reale@hotmail.com>
2023-06-16 23:55:43 -0300

    Revert "Hunter Changes"
    This reverts commit 4ab10de0f5eb492dd605aede4b1317742c4a37cd.

--- Assets/Scripts/Hunter/Hunter.cs
+++ Assets/Scripts/Hunter/Hunter.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Linq;
+
 public class Hunter : MonoBehaviour
 {
     private FiniteStateMachine _finiteStateMachine;
@@ -50,23 +50,8 @@ public class Hunter : MonoBehaviour
     public void CheckProximity()
     {
         Collider2D[] proximity = Physics2D.OverlapCircleAll(transform.position, proximityRadius);
-        //proximity.PrintCollection();
-        var bTarget = proximity.Select(x => x.GetComponent<FlockAgent>()).Where(x => x != null);//.OrderBy(x => Vector3.Distance(transform.position, x.transform.position)).ToList().FirstOrDefault();
-        bTarget.PrintCollection();
-        /*if(bTarget != null)
-        {
-            _target = bTarget.transform;
-            targetAcquiredFlag = true;
-        }
-        else
-        {
-            targetAcquiredFlag = false;
-        }
-        if (bTarget == null)
-        {
-            proximityRadius += 1f;
-        }*/
-        /*foreach (Collider2D collider in proximity)
+
+        foreach (Collider2D collider in proximity)
         {
             FlockAgent target = collider.GetComponent<FlockAgent>();
 
@@ -83,8 +68,8 @@ public class Hunter : MonoBehaviour
             if (target == null)
             {
                 proximityRadius += 1f;
-            
-         }*/
+            }
+        }
     }
 
     public void Persuit(Vector3 _velocity)
@@ -105,6 +90,7 @@ public class Hunter : MonoBehaviour
         {
             Persuit(CalculateTrajectory(_target));
         }
+
     }
     public Vector3 CalculateTrajectory(Transform target) {
 
@@ -115,6 +101,7 @@ public class Hunter : MonoBehaviour
             desired *= speed;
             return desired;
         }
+
         return default;
     }
     public void SetPatrolBehaviour()
