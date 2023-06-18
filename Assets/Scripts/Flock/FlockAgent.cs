5ddcc5de1a73c8d6581800a5c0d783f050c7dced
Nicolas Castagnola <castagnola.nicolas@gmail.com>
2023-06-18 17:54:48 -0300

    optimiza

--- Assets/Scripts/Flock/FlockAgent.cs
+++ Assets/Scripts/Flock/FlockAgent.cs
@@ -5,25 +5,28 @@ public class FlockAgent : GridEntity
 {
     [HideInInspector] public Flock agentFlock;
     public Flock AgentFlock => agentFlock;
-    public Collider2D AgentCollider { get; private set; }
-    private void Start() => AgentCollider = GetComponent<Collider2D>();
+    // public Collider2D AgentCollider { get; private set; }
+    // private void Start() => AgentCollider = GetComponent<Collider2D>();
     public void Initialize(Flock flock) => agentFlock = flock;
     public void Kill() => agentFlock.RemoveAgentFromList(this);
     public void Move(Vector2 velocity)
     {
-        transform.up = velocity;
-        transform.position += (Vector3)velocity * Time.deltaTime;
+        var agentTransform = transform;
+        
+        agentTransform.up = velocity;
+        agentTransform.position += (Vector3)velocity * Time.deltaTime;
 
         MoveCallback();
     }
-
-    public List<GridEntity> GetNearby()
+    public IEnumerable<GridEntity> GetNearby()
     {
-        //humo
         if (agentFlock == null) return null;
+
+        var position = transform.position;
+        
         return agentFlock._spatialGrid.Query(
-                transform.position + new Vector3(-agentFlock.neighborRadius, -agentFlock.neighborRadius, 0),
-                transform.position + new Vector3(agentFlock.neighborRadius, agentFlock.neighborRadius, 0),
+                position + new Vector3(-agentFlock.neighborRadius, -agentFlock.neighborRadius, 0),
+                position + new Vector3(agentFlock.neighborRadius, agentFlock.neighborRadius, 0),
                 x => {
                     var position2d = x - transform.position;
                     position2d.z = 0;
