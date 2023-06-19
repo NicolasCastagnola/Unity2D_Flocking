21e5e78b4cf4a7ca13df6efa44c78933ab5d215a
Nicolas Castagnola <castagnola.nicolas@gmail.com>
2023-06-19 01:05:08 -0300

    ui funcionando

--- Assets/Scripts/UserInterfaceManager.cs
+++ Assets/Scripts/UserInterfaceManager.cs
@@ -7,7 +7,7 @@ public class UserInterfaceManager : BaseMonoSingleton<UserInterfaceManager>
     private static readonly int Hide = Animator.StringToHash("Hide");
     private bool isVisible = true;
     
-    [SerializeField] private List<UI_CollapsableContainer> _collapsableContainers;
+    [SerializeField] private List<AnimatedContainer> _collapsableContainers;
     [SerializeField] private Animator _animator;
 
     public void TriggerAnimator()
@@ -17,16 +17,16 @@ public class UserInterfaceManager : BaseMonoSingleton<UserInterfaceManager>
         _animator.Play(isVisible ? Hide : Show);
     }
 
-    public void CollapseContainer(UI_CollapsableContainer targetContainer)
+    public void CollapseContainer(AnimatedContainer targetContainer)
     {
         if (_collapsableContainers.Count == 0) return;
 
         foreach (var container in _collapsableContainers.Where(container => targetContainer == container))
         {
-            container.Collapse();
+            container.Close();
         }
     }
-    public void OpenContainer(UI_CollapsableContainer targetContainer)
+    public void OpenContainer(AnimatedContainer targetContainer)
     {
         if (_collapsableContainers.Count == 0) return;
         
@@ -34,7 +34,7 @@ public class UserInterfaceManager : BaseMonoSingleton<UserInterfaceManager>
         {
             if (targetContainer == container) container.Open();
 
-            else container.Collapse();
+            else container.Close();
         }
     }
 }
